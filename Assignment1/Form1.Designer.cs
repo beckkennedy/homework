64ff409fcf9b324e240fc132f799a76e338ccd40
beck <kennedr6@gmatc.matc.edu>
2020-02-02 22:50:17 -0600

    Assignment 1

---
+++ Assignment1/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Assignment1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.labelError = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAdd.Location = new System.Drawing.Point(68, 527);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(126, 48);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(68, 479);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(298, 30);
+            this.textBox1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(68, 133);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(298, 279);
+            this.listBox1.TabIndex = 3;
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRemove.Location = new System.Drawing.Point(240, 527);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(126, 48);
+            this.buttonRemove.TabIndex = 4;
+            this.buttonRemove.Text = "Remove";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelError.Location = new System.Drawing.Point(63, 435);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(0, 25);
+            this.labelError.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(72, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(282, 55);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Grocery List";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 610);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelError);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonAdd);
+            this.Name = "Form1";
+            this.Text = "Grocery List App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Label labelError;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Assignment1/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment1
+{
+    public partial class Form1 : Form
+    {
+
+        //C:\Users\beck\Source\Repos\Assignment1\Assignment1\bin\Debug
+        string fileName = "list.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+            if (File.Exists(fileName))
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader(fileName))
+                    {
+                        string line;
+                        
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            listBox1.DataSource = File.ReadAllLines(fileName);
+                        }
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    labelError.Text = "The file could not be read:";
+                    Console.WriteLine(ex.Message);
+                }
+            } else
+            {
+                labelError.Text = "ERROR: File does not exist.";
+            }
+         
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Equals(""))
+            {
+                labelError.Text = "ERROR: Type in item you want to add.";
+            }
+            else
+            {
+                string alreadyAdded = textBox1.Text;
+                var list = File.ReadAllLines(fileName);
+                if (list.Contains(alreadyAdded))
+                {
+                    labelError.Text = "ERROR: Item already exists";
+                }
+                else
+                {
+                    using (StreamWriter sw = File.AppendText(fileName))
+                    {
+                        sw.WriteLine(textBox1.Text);
+                        labelError.Text = "Added Successfully!";
+                    }
+                }
+
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    string line;
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        listBox1.DataSource = File.ReadAllLines(fileName);
+                    }
+                }
+
+                //clear textbox after entry
+                textBox1.Text = "";
+            }
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            
+            string deleteThis = textBox1.Text;
+            var list = File.ReadAllLines(fileName);
+            if (list.Contains(deleteThis))
+            {
+                var newList = list.Where(line => !line.Contains(deleteThis));
+                File.WriteAllLines(fileName, newList);
+
+                listBox1.DataSource = File.ReadAllLines(fileName);
+
+                labelError.Text = "Removed Successfully!";
+            }
+
+            else
+            {
+                if (textBox1.Text.Equals(""))
+                {
+           
+                    labelError.Text = "ERROR: Type in item you want to delete.";
+                }
+
+                else
+                {
+                    labelError.Text = "ERROR: Item Not Found";
+                }
+            }
+
+            textBox1.Text = "";
+            //textBox1.Text = Environment.CurrentDirectory;
+            //Console.WriteLine(Environment.CurrentDirectory);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
